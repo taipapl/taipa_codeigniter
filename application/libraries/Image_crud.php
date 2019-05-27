94eafc44acf1fdbb9589483397d62f448a22fe37
Michal Myslinski <michal@taipa.pl>
2019-05-27 13:28:58 +0200

    rename class

--- application/libraries/Image_crud.php
+++ application/libraries/Image_crud.php
@@ -19,7 +19,7 @@
  * @version    	0.5
  * @author     	John Skoumbourdis <scoumbourdisj@gmail.com>
  */
-class Image_CRUD {
+class Image_crud {
 
 	protected $table_name = null;
 	protected $priority_field = null;
